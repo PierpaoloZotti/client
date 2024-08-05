f58dd70dfdf2d71ddcc01bcb350f20cda39d48b7
Pierpaolo Zotti <zotti.pierpaolo@outlook.it>
2024-08-05 06:42:56 -0300

    chore: create provider for redux

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,17 +1,39 @@
-import Navbar from '@/app/dashboard/_components/navbar';
-import React from 'react';
+'use client';
 
-const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+import Navbar from '@/app/dashboard/_components/navbar';
+import Sidebar from '@/app/dashboard/_components/sidebar';
+import React from 'react';
+import StoreProvider, { useAppSelector } from './redux';
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSideBarCollapsed,
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
   return (
-    <div className={`light flex w-full min-h-screen bg-gray-50 text-gray-900`}>
-      Sidebar
+    <div
+      className={`${
+        isDarkMode ? 'dark' : 'light'
+      } flex w-full min-h-screen bg-gray-50 text-gray-900`}
+    >
+      <Sidebar />
       <main
-        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}
+        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${
+          isSidebarCollapsed ? 'md:pl-24' : 'md:pl-72'
+        }`}
       >
         <Navbar />
         {children}
       </main>
     </div>
+  );
+};
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
   );
 };
 
